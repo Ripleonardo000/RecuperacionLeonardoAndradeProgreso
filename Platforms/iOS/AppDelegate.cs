2a8467f3493e029552e6b04b7e464bb9bba77295
Ripleonardo000 <leonardo.andrade@udla.edu.ec>
2024-07-24 10:50:14 -0500

    Agregar archivos de proyecto.

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace RecuperacionLeonardoAndradeProgreso
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
